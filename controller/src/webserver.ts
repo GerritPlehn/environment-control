f2608c8d96bbd5380699b50fa0e371df08a088c7
Gerrit Plehn <gerrit@plehn.me>
2025-04-20 13:36:53 +0200

    expose vpd and mode on api

--- controller/src/webserver.ts
+++ controller/src/webserver.ts
@@ -6,8 +6,11 @@ const app = new Hono();
 app.get('/', (c) =>
   c.json({
     settings: {
+      mode: env.MODE,
       minHumidity: env.MIN_HUMIDITY,
       maxHumidity: env.MAX_HUMIDITY,
+      minVPD: env.MIN_VPD,
+      maxVPD: env.MAX_VPD,
     },
     hygroData,
   })
