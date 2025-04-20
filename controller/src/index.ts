68e2bf7813ed1c1b2dbb292b63ac8db2eac71ada
Gerrit Plehn <gerrit@plehn.me>
2025-04-20 13:21:04 +0200

    implement VPD mode

--- controller/src/index.ts
+++ controller/src/index.ts
@@ -3,21 +3,44 @@ import { hygroData } from './hygroLogger.ts';
 import { env } from './env.ts';
 import { serve } from '@hono/node-server';
 import app from './webserver.ts';
-serve({ fetch: app.fetch, port: 3000 });
+import { assertNever } from './util.ts';
+
+serve({ fetch: app.fetch, port: env.PORT });
 
 async function hygroControl() {
-  console.log(hygroData);
-
-  if (hygroData.humidity < env.MIN_HUMIDITY) {
-    console.log('turn on humidifier');
-    // await fetch(`${env.SHELLY_URL}/relay/0?turn=on`);
-  }
-  if (hygroData.humidity > env.MAX_HUMIDITY) {
-    console.log('turn off humidifier');
-    // await fetch(`${env.SHELLY_URL}/relay/0?turn=off`);
+  try {
+    switch (env.MODE) {
+      case 'HUMIDITY': {
+        if (hygroData.humidity < env.MIN_HUMIDITY) {
+          console.log('turn on humidifier');
+          await fetch(`${env.SHELLY_URL}/relay/0?turn=on`);
+        }
+        if (hygroData.humidity > env.MAX_HUMIDITY) {
+          console.log('turn off humidifier');
+          await fetch(`${env.SHELLY_URL}/relay/0?turn=off`);
+        }
+        break;
+      }
+      case 'VPD': {
+        if (hygroData.vpd < env.MIN_VPD) {
+          console.log('turn off humidifier');
+          await fetch(`${env.SHELLY_URL}/relay/0?turn=off`);
+        }
+        if (hygroData.vpd > env.MAX_VPD) {
+          console.log('turn on humidifier');
+          await fetch(`${env.SHELLY_URL}/relay/0?turn=on`);
+        }
+        break;
+      }
+      default: {
+        assertNever(env.MODE);
+      }
+    }
+  } catch (error) {
+    console.error('failed to switch shelly output!');
   }
 }
-setInterval(hygroControl, 1000 * 60);
+setInterval(hygroControl, 1000 * env.CHECK_INTERVAL_SEC);
 
 const gracefulShutdown = async () => {
   await mqttClient.endAsync();
